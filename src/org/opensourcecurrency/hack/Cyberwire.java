0ced1c221bfe190068f92cd2b5ad510bfc05b33e
Tom Brown <herestomwiththeweather@gmail.com>
2012-04-14 03:12:34 -0500

    get authorization code

--- src/org/opensourcecurrency/hack/Cyberwire.java
+++ src/org/opensourcecurrency/hack/Cyberwire.java
@@ -1,13 +1,19 @@
 package org.opensourcecurrency.hack;
 
+import org.opensourcecurrency.hack.WebViewActivity;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Cyberwire extends Activity {
 	private static final String TAG = "OpenTransact";
@@ -34,11 +40,35 @@ public class Cyberwire extends Activity {
     	case R.id.exit:
     		return true;
     	case R.id.login:
+    		Intent intent = new Intent(this,WebViewActivity.class);
+    		//intent.setData(Uri.parse("http://demo.opensourcecurrency.org/oauth/authorize?client_id=xBoHeeNNFt3LQ7U1tvAb8BVKr32duE6rdWtpCSFD&response_type=code"));
+    		intent.setData(Uri.parse("http://192.168.1.26:3000/oauth/authorize?client_id=XH/Jov43AaMoE08mUtRvoQ==&response_type=code"));
+    		startActivityForResult(intent,0);
     		return true;
     	case R.id.settings:
     		startActivity(new Intent(this, Prefs.class));
     		return true;
     	}
     	return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	Log.d(TAG,"onActivityResult");
+
+    	switch (requestCode) {
+    	case 0:
+    		if (resultCode != RESULT_OK || data == null) {
+    			return;
+    		}
+    		String token = data.getStringExtra("token");
+    		Log.d(TAG,token);
+    		Toast toast = Toast.makeText(this, token, Toast.LENGTH_LONG);
+    		toast.setGravity(Gravity.CENTER, 0, 0);
+    		toast.show();
+
+    		return;
+    	}
+    	super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ src/org/opensourcecurrency/hack/WebViewActivity.java
@@ -0,0 +1,58 @@
+package org.opensourcecurrency.hack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.util.Log;
+
+public class WebViewActivity extends Activity {
+	private static final String TAG = "OpenTransact";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_PROGRESS);
+		
+		WebView webview = new WebView(this);
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public void onProgressChanged(WebView view, int progress) {
+				setProgress(progress * 100);
+			}
+		});
+		webview.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				setTitle(url);
+			}
+			
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				Log.d(TAG,"onPageFinished");
+
+				Uri uri = Uri.parse(url);
+				String code = uri.getQueryParameter("code");
+				if (code != null) {
+				  Intent result = new Intent();
+				  result.putExtra("token", code);
+				  setResult(RESULT_OK, result);
+				  finish();
+				}
+			}
+		});
+		setContentView(webview);
+		
+		Intent intent = getIntent();
+		if (intent.getData() != null) {
+			webview.loadUrl(intent.getDataString());
+		}
+	}
+
+}
