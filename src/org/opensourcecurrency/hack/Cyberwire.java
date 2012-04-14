9497e1688d4edb2ffff722ad447e0f75d430ac78
Tom Brown <herestomwiththeweather@gmail.com>
2012-04-14 14:55:02 -0500

    use asset provider selected in preferences. fetch access, refresh token.

--- src/org/opensourcecurrency/hack/Cyberwire.java
+++ src/org/opensourcecurrency/hack/Cyberwire.java
@@ -1,12 +1,28 @@
 package org.opensourcecurrency.hack;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.opensourcecurrency.hack.RestTask;
 import org.opensourcecurrency.hack.WebViewActivity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import android.util.Log;
 import android.view.Gravity;
@@ -16,13 +32,26 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class Cyberwire extends Activity {
+	private static final String OAUTH_TOKEN_ACTION = "org.opensourcecurrency.hack.OAUTH_TOKEN";
 	private static final String TAG = "OpenTransact";
+	private ProgressDialog progress;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+		SharedPreferences tokenStore;
+		tokenStore = getPreferences(Activity.MODE_PRIVATE);
+		String access_token = tokenStore.getString("access_token","none");
+		String expires_in = tokenStore.getString("expires_in","none");
+
+    	Log.d(TAG,"onCreate: " + expires_in + " : " + access_token);
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+		Toast toast = Toast.makeText(this, "expires_in: " + expires_in + " : " + access_token, Toast.LENGTH_LONG);
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		toast.show();
     }
     
     @Override
@@ -35,14 +64,19 @@ public class Cyberwire extends Activity {
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+    	String providerValues[];
     	Log.d(TAG,"onOptionsItemSelected");
     	switch (item.getItemId()) {
     	case R.id.exit:
     		return true;
     	case R.id.login:
     		Intent intent = new Intent(this,WebViewActivity.class);
-    		//intent.setData(Uri.parse("http://demo.opensourcecurrency.org/oauth/authorize?client_id=xBoHeeNNFt3LQ7U1tvAb8BVKr32duE6rdWtpCSFD&response_type=code"));
-    		intent.setData(Uri.parse("http://192.168.1.26:3000/oauth/authorize?client_id=XH/Jov43AaMoE08mUtRvoQ==&response_type=code"));
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    		String provider = prefs.getString("assetProviderPref","");
+        	providerValues = provider.split(" ");
+        	Log.d(TAG,"onOptionsItemSelected[login] host: " + providerValues[0]);
+        	Log.d(TAG,"onOptionsItemSelected[login] clientid: " + providerValues[1]);
+    		intent.setData(Uri.parse(providerValues[0]+"/oauth/authorize?client_id="+providerValues[1]+"&response_type=code"));
     		startActivityForResult(intent,0);
     		return true;
     	case R.id.settings:
@@ -54,6 +88,7 @@ public class Cyberwire extends Activity {
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	String providerValues[];
     	Log.d(TAG,"onActivityResult");
 
     	switch (requestCode) {
@@ -66,9 +101,85 @@ public class Cyberwire extends Activity {
     		Toast toast = Toast.makeText(this, token, Toast.LENGTH_LONG);
     		toast.setGravity(Gravity.CENTER, 0, 0);
     		toast.show();
+    		
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    		String provider = prefs.getString("assetProviderPref","");
+        	providerValues = provider.split(" ");
+        	Log.d(TAG,"onActivityResult host: " + providerValues[0]);
+        	Log.d(TAG,"onActivityResult clientid: " + providerValues[1]);
+        	Log.d(TAG,"onActivityResult client secret: " + providerValues[2]);
+        	Log.d(TAG,"onActivityResult redirect uri: " + providerValues[3]);
 
+        	
+       		try {
+      		  String url = providerValues[0] + "/oauth/token";
+      		  HttpPost tokenRequest = new HttpPost(new URI(url));
+      		  List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+      		  parameters.add(new BasicNameValuePair("client_id", providerValues[1]));
+      		  parameters.add(new BasicNameValuePair("client_secret", providerValues[2]));
+      		  parameters.add(new BasicNameValuePair("code", token));
+      		  parameters.add(new BasicNameValuePair("grant_type","authorization_code"));
+      		  parameters.add(new BasicNameValuePair("redirect_uri",providerValues[3]));
+      		  tokenRequest.setEntity(new UrlEncodedFormEntity(parameters));
+      		  
+      		  RestTask task = new RestTask(this, OAUTH_TOKEN_ACTION);
+      		  task.execute(tokenRequest);
+            	  progress = ProgressDialog.show(this, "Fetching token", "Waiting...", true);
+
+      		} catch (Exception e) {
+      			e.printStackTrace();
+      		}
     		return;
     	}
     	super.onActivityResult(requestCode, resultCode, data);
     }
+    
+    @Override
+    public void onResume() {
+      super.onResume();
+      registerReceiver(receiver, new IntentFilter(OAUTH_TOKEN_ACTION));
+    }
+    
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	unregisterReceiver(receiver);
+    }
+    
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+    	@Override
+    	public void onReceive(Context context, Intent intent) {
+    		Log.d(TAG,"onReceive called!");
+    		if(progress != null) {
+    			progress.dismiss();
+    		}
+    		String response = intent.getStringExtra(RestTask.HTTP_RESPONSE);
+    		Log.d(TAG,"response: "+response);
+    		
+    		try {
+    		  SharedPreferences tokenStore;
+    		  tokenStore = getPreferences(Activity.MODE_PRIVATE);
+              JSONObject access_token_response = new JSONObject(response);
+        	  String access_token = access_token_response.getString("access_token");
+        	  String refresh_token = access_token_response.getString("refresh_token");
+        	  String expires_in = access_token_response.getString("expires_in");
+
+        	  Log.d(TAG," access token: " + access_token);
+        	  Log.d(TAG,"refresh token: " + refresh_token);
+        	  Log.d(TAG,"   expires_in: " + expires_in);
+
+
+              SharedPreferences.Editor editor = tokenStore.edit();
+              editor.putString("access_token", access_token);
+              editor.putString("refresh_token", refresh_token);
+              editor.putString("expires_in", expires_in);
+
+              editor.commit();
+
+    		} catch (JSONException e) {
+    		  e.printStackTrace();
+    		}
+
+    	}
+    };
 }
---
+++ src/org/opensourcecurrency/hack/RestTask.java
@@ -0,0 +1,56 @@
+package org.opensourcecurrency.hack;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+public class RestTask extends AsyncTask<HttpUriRequest, Void, String> {
+
+    public static final String HTTP_RESPONSE = "httpResponse";
+	
+    private Context mContext;
+    private HttpClient mClient;
+    private String mAction;
+	
+    public RestTask(Context context, String action) {
+        mContext = context;
+        mAction = action;
+        mClient = new DefaultHttpClient();
+    }
+	
+    public RestTask(Context context, String action, HttpClient client) {
+        mContext = context;
+        mAction = action;
+        mClient = client;
+    }
+	
+    @Override
+    protected String doInBackground(HttpUriRequest... params) {
+        try{
+            HttpUriRequest request = params[0];
+            HttpResponse serverResponse = mClient.execute(request);
+			
+            BasicResponseHandler handler = new BasicResponseHandler();
+            String response = handler.handleResponse(serverResponse);
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+	
+    @Override
+    protected void onPostExecute(String result) {
+        Intent intent = new Intent(mAction);
+        intent.putExtra(HTTP_RESPONSE, result);
+        //Broadcast the completion
+        mContext.sendBroadcast(intent);
+    }
+
+}
