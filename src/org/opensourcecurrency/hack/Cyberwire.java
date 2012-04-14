c5afef9d4fc0f821b4808750f519c063d5d19404
Tom Brown <herestomwiththeweather@gmail.com>
2012-04-14 00:42:37 -0500

    added menu. settings to select asset provider

--- src/org/opensourcecurrency/hack/Cyberwire.java
+++ src/org/opensourcecurrency/hack/Cyberwire.java
@@ -1,13 +1,44 @@
 package org.opensourcecurrency.hack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 public class Cyberwire extends Activity {
+	private static final String TAG = "OpenTransact";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	Log.d(TAG,"onOptionsItemSelected");
+    	switch (item.getItemId()) {
+    	case R.id.exit:
+    		return true;
+    	case R.id.login:
+    		return true;
+    	case R.id.settings:
+    		startActivity(new Intent(this, Prefs.class));
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
     }
 }
---
+++ src/org/opensourcecurrency/hack/Prefs.java
@@ -0,0 +1,12 @@
+package org.opensourcecurrency.hack;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Prefs extends PreferenceActivity {
+	   @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      addPreferencesFromResource(R.xml.settings);
+	   }
+}
